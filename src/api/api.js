dbf4c2322c7b82c9595fa552f706bc2f819cf0d9
nikitadeora-glitch <nikita.deora@prpwebs.in>
2025-12-31 16:46:43 +0530

    changed base url

--- src/api/api.js
+++ src/api/api.js
@@ -2,12 +2,13 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
   withCredentials: true,
 });
+
 
 // Token management
 const getToken = () => localStorage.getItem('authToken');
