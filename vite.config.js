94fc7138573d80f0262665bc3352942d84a70c9c
nikitadeora-glitch <nikita.deora@prpwebs.in>
2026-01-05 14:52:23 +0530

    add both base url local or live

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
   },
   define: {
     'import.meta.env.VITE_API_URL': JSON.stringify('http://127.0.0.1:8000/api'),
+    'import.meta.env.VITE_API_URL': JSON.stringify('https://api.seo.prpwebs.com/api'),
     'import.meta.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify('276575813186-e1q7equvp7nq222q5sbal2f7aaensau3.apps.googleusercontent.com')
   }
 })
